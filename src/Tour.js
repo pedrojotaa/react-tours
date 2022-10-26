9a8d1865b5224675d9f1babe3bfb5f0ca77da906
“João <eng.joao.pedro@outlook.com>
2022-10-25 23:09:56 -0300

    refactor: add toggle btn

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,10 @@
 import styles from "./Tour.module.css";
 
+import { useState } from "react";
+
 function Tour({ id, image, info, price, name }) {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className={styles.tour_container}>
       <img src={image} alt={name}></img>
@@ -9,7 +13,10 @@ function Tour({ id, image, info, price, name }) {
           <h4>{name}</h4>
           <h4 className={styles.tour_price}>R$ {price}</h4>
         </div>
-        <p>{info}</p>
+        <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? "show less" : "read more"}
+        </button>
         <button>not interested</button>
       </footer>
     </article>
